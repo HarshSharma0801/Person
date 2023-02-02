47a17a5b4a6f80bf78824544919fe04a317c39f0
HarshSharma0801 <121893196+HarshSharma0801@users.noreply.github.com>
2023-02-02 22:55:01 +0530

    Adding My Name

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,12 @@ public class Main {
         person.setAge(-5);
         System.out.println("FullName= "+person.getFullName());
         System.out.println("teen= "+person.isTeen());
+        person.setFirstName("Harsh");
+        person.setLastName("Sharma");
+        System.out.println("FullName= "+person.getFullName());
+
+        person.setAge(19);
+        System.out.println("teen= "+person.isTeen());
 
     }
 }
